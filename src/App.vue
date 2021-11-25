c7512c5b4d323b5192f8c292689a55e7a8d29496
fabiano1133 <fabiano.santos0@icloud.com>
2021-11-24 21:09:28 -0300

    Inicio do Projeto

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <h1>Calculadora</h1>
+    <Calculator />
+  </div>
+</template>
+
+<script>
+
+import Calculator from './main/Calculator'
+
+export default {
+  components: { Calculator }
+}
+</script>
+
+<style>
+  @font-face {
+    font-family: "RobotoMono" ;
+    src: url("./fonts/RobotoMono-Thin.ttf");
+  }
+
+  * {
+    font-family: "RobotoMono", monospace ;
+  }
+
+  body {
+    margin: 0
+
+  }
+
+  #app {
+    display: flex;
+    flex-direction: column;
+    height:100vh;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+
+    color: #fff;
+    background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,121,82,1) 35%, rgba(0,212,255,1) 100%);
+  }
+</style>
